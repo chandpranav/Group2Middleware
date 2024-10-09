e934ef6c2dec3f941ed33064dfcaa712c01452e0
Diegov5498 <Diegov5498@gmail.com>
2024-10-08 19:16:36 -0700

    Fix middleware

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,12 +5,12 @@ import {
   updateUserData,
   deleteUserData,
 } from "../controller/user.controller.js"; // Import controller functions
-// import checkUser from "../middleware/checkuser.middleware.js"; // Import your middleware
+import checkUser from "../middleware/checkuser.middleware.js"; // Import your middleware
 
 const userRoute = Router();
 
 // Define all the routes for user-related operations
-userRoute.get("/:email", getUserData); // Read user by email
+userRoute.get("/:email", checkUser, getUserData); // Read user by email
 userRoute.post("/", createUser); // Create a new user
 userRoute.patch("/:email", updateUserData); // Update user by email
 userRoute.delete("/:email", deleteUserData); // Delete user by email
